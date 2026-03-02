cebc8e4f914e6032c0b2350bc35f31d48d6964da
chaoslee <chaos2lee@qq.com>
2026-03-02 11:49:05 +0800

    @produck/eslint-rules: [FIXED] d.ts: comments [FIXED] tsconfig.json [FIXED] package.json: `.exports`

--- packages/eslint-rules/src/index.d.ts
+++ packages/eslint-rules/src/index.d.ts
@@ -1,4 +1,19 @@
 import * as EsLint from 'eslint';
 
+/**
+ * Pre-defined ESLint flat config object containing
+ * all Produck team coding conventions.
+ */
 export const config: EsLint.Linter.Config;
-export function excludeGitIgnore(eslintConfigPath: string): EsLint.Linter.Config;
+
+/**
+ * Generate an ESLint config that excludes paths
+ * listed in the project's `.gitignore` file.
+ *
+ * @param eslintConfigPath - Pass `import.meta.url`
+ *   to locate the `.gitignore` file relative to
+ *   the project root.
+ */
+export function excludeGitIgnore(
+	eslintConfigPath: string,
+): EsLint.Linter.Config;
